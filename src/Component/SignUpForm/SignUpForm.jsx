773bfe54b16cbbe8b9a6d28713d369e8991565ca
Faruks23 <mdfaruksp2@gmail.com>
2023-10-29 01:13:51 +0600

    implement create user by email and password using firebase

--- src/Component/SignUpForm/SignUpForm.jsx
+++ src/Component/SignUpForm/SignUpForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useContext, useRef, useState } from 'react';
 import styled from 'styled-components';
  import {
    FormContainer,
@@ -11,33 +11,95 @@ import styled from 'styled-components';
    LoginButton,
    Terms,
  } from "../Style";
+import { AuthContext } from '../AutProvider/AutProvider';
+import { useNavigate } from 'react-router-dom';
+
+
 
 const SignUpForm = () => {
+
+  const { CreateUser } = useContext(AuthContext);
    const [showPassword, setShowPassword] = useState(false);
-   const [ToggleForm,setToggleForm] = useState(false);
+  const [ToggleForm, setToggleForm] = useState(false);
+   
+  const EmailRef=useRef()
+  const PasswordRef=useRef()
+  const ConfirmPasswordRef = useRef()
+  
+  const Navigation=useNavigate()
+ 
+
+
    const handlePasswordVisibility = () => {
      setShowPassword(!showPassword);
    };
 
 
-  const handleSignUp = () => { 
 
+
+  const handleSignUp = (e) => { 
+    e.preventDefault();
+    // console.log(EmailRef.current.value);
+  
+    const Email = EmailRef.current.value;
+
+   const Password = PasswordRef.current.value;
+   const confirmPassword = ConfirmPasswordRef.current.value;
+    
+//     // reexpression
+//  if (password !== ConfirmPassword) {
+//    toast.error("Please enter right password");
+//    return;
+//  } else if (password.length < 6) {
+//    toast.error("password must be Greater than 6 characters");
+//    return;
+//  } else if (!/[A-Z]/.test(password)) {
+//    toast.error("Error: password must  have a capital letter.");
+//    return;
+//  } else if (!/[!@#$%^&*()-=_+|;':",.<>/?]/.test(password)) {
+//    toast.error("Error: password must  have a special character.");
+//    return;
+//  }
+
+    // create new user with new password and email
+    if (Email && Password) {
+      CreateUser(Email, Password)
+        .then((result) => {
+          const user = result.user;
+          console.log(user);
+          Navigation('/')
+          alert("Registration successful");
+        })
+        .catch((err) => {
+          console.log(err);
+        alert(err.message);
+        });
+      
+    }
+ 
 
   }
 
 
 
   return (
-    <FormContainer>
+    <FormContainer onSubmit={handleSignUp}>
       <LoginFormTitle>SignUp</LoginFormTitle>
       <Label>Email</Label>
-      <LoginFormInput placeholder="Login ID" />
+      <LoginFormInput
+        placeholder="Enter Your Email"
+        type="email"
+        name="email"
+       ref={EmailRef}
+      />
       <Label>Password</Label>
 
       <PasswordContainer>
         <LoginFormInput
           type={showPassword ? "password" : "text"}
           placeholder="Password"
+          name="password"
+          ref={PasswordRef}
         />
         <PasswordVisibilityIcon onClick={handlePasswordVisibility}>
           {showPassword ? "👁️" : "👁️"}
@@ -47,6 +109,8 @@ const SignUpForm = () => {
         <LoginFormInput
           type={showPassword ? "password" : "text"}
           placeholder=" Confirm Password"
+          name="ConfirmPassword"
+          ref={ConfirmPasswordRef}
         />
         <PasswordVisibilityIcon onClick={handlePasswordVisibility}>
           {showPassword ? "👁️" : "👁️"}
@@ -56,7 +120,9 @@ const SignUpForm = () => {
       <CheckboxLabel>
         <input type="checkbox" /> Agree to <Terms> Terms & conditions</Terms>
       </CheckboxLabel>
-      <LoginButton onClick={handleSignUp}>Submit</LoginButton>
+      <LoginButton type="submit" onClick={handleSignUp}>
+        Submit
+      </LoginButton>
     </FormContainer>
   );
 };
--- src/Component/Style.jsx
+++ src/Component/Style.jsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-export const FormContainer = styled.div`
+export const FormContainer = styled.form`
   width: 100%;
   max-width: 550px;
   text-align: center;
